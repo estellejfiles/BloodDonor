6fc9bd31c04003f275ca8476321a80d13c626f9d
Estelle <epfiles@blah.com>
2025-12-09 23:41:19 -0600

    Initial setting up: part 2

--- src/main/java/com/estelle/blooddonor/BloodDonorController.java
+++ src/main/java/com/estelle/blooddonor/BloodDonorController.java
@@ -1,4 +1,18 @@
 package com.estelle.blooddonor;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// controller = collection of methods used to return info to user as response to their activity/input
+@Controller
 public class BloodDonorController {
+
+    @GetMapping
+    public String showDonorForm(Model model) {
+        // create new donor object (it is empty right now)
+        model.addAttribute("donor", new Donor());
+        // return template
+        return "donor_form";
+    }
 }
--- src/main/java/com/estelle/blooddonor/Donor.java
+++ src/main/java/com/estelle/blooddonor/Donor.java
@@ -1,4 +1,36 @@
 package com.estelle.blooddonor;
 
 public class Donor {
+    // define characteristics for fields the donor has
+    private String firstName;
+    int age;
+    int weight;
+
+    // constructor w/o arguments; Spring will create donor object w/ getters & setters
+    public Donor () {}
+
+    // getters & setters that get data when called & set it as the associated variable's value
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
